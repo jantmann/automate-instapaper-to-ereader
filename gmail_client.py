bca9cb9c549a8e2f5c9cd051f749a95c2e876c43
Jake <jaa4xh@virginia.edu>
2026-05-08 11:33:53 -0400

    fixing refactor, no substack sender, updating requirements file

--- gmail_client.py
+++ gmail_client.py
@@ -39,7 +39,7 @@ def get_gmail_service():
 
     return build("gmail", "v1", credentials=creds)
 
-def get_unread_substack_emails(service, sender_email):
+def get_unread_substack_emails(service):
     cutoff = datetime.datetime.now() - datetime.timedelta(hours=26) ## Look back 26 hours to ensure we catch all recent emails
     after_timestamp = int(cutoff.timestamp())
     query = f"from:@substack.com -from:no-reply@substack.com is:unread after:{after_timestamp}"
